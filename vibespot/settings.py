45d2027de91808d88a73c23713efc3cf9f01a6a9
Azeez Akinsola <harkzed@gmail.com>
2018-12-02 21:52:55 +0100

    first commit

--- vibespot/settings.py
+++ vibespot/settings.py
@@ -83,12 +83,23 @@ WSGI_APPLICATION = 'vibespot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vibespotdb',
+        'USER': 'vibespot',
+        'PASSWORD': 'VibespotIntemporel',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-}
+
 
 
 # Password validation
